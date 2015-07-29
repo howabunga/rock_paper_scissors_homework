5888c48bb40e6f53426ad97b05d4904170cf7f42
howabunga <weihowa@gmail.com>
2015-07-29 00:40:15 -0500

    Rock paper scissors homework

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,58 @@
 class GameController < ApplicationController
   # This action is for the bare domain. You can ignore it.
-  def home
-    redirect_to("/mockup.html")
+
+  def welcome
+
+    render("welcome.html.erb")
+
   end
 
-  # Your code goes below.
+  def rps
 
+    @user_move = params["move"]
+
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @user_move == @computer_move
+      @outcome = "tied"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @outcome = "won"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @outcome = "lost"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @outcome = "lost"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @outcome = "won"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @outcome = "lost"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @outcome = "won"
+    end
+
+    m = Move.new
+    m.user_move = @user_move
+    m.computer_move = @computer_move
+    if @outcome == "won"
+      m.user_wins = 1
+      m.computer_wins = 0
+      m.tie = 0
+    elsif @outcome == "lost"
+      m.user_wins = 0
+      m.computer_wins = 1
+      m.tie = 0
+    else
+      m.user_wins = 0
+      m.computer_wins = 0
+      m.tie = 1
+
+    end
+
+    m.save
+
+    @all_moves = Move.all
+
+    render("test.html.erb")
+
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # This route is for the bare domain. You can ignore it.
-  get("/", { :controller => "game", :action => "home" })
 
-  # Your code goes below.
+  get("/play/:move", { :controller => "game", :action => "rps" })
+  get("/", { :controller => "game", :action => "welcome" })
 
 end
